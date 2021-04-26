0b412ee0ee314e18ace77457f8e74764b2a7f4ea
Tami Fedrigo <72176139+tamifedrigo@users.noreply.github.com>
2021-04-26 19:02:07 -0400

    Add files via upload
    "first commit"

---
+++ script.js
@@ -0,0 +1,27 @@
+//GET AREA OF CIRCLE
+function getAreaOfCircle(radius) {
+    if(radius >= 0) {
+    return Math.PI = (radius * radius);
+}
+}
+console.log(getAreaOfCircle(radius(5));
+
+
+//GET CIRCUMFERENCE OF CIRCLE
+function getCircumferenceOfCircle(radius2) {
+    return 2 * 3.14 * radius2;
+}
+getCircumferenceOfCircle(5);
+
+//AREA OF SQUARE - WORKING
+function getAreaOfSquare(side) {
+    area = side * side;
+    return area;
+}
+let area = 5
+console.log(getAreaOfSquare(5));
+
+//AREA OF TRIANGLE
+function getAreaofTriangle(base, height) {
+    return base * height * .5;
+}
